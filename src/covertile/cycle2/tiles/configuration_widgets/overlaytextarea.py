7e6272b3c57ae21cb0d472a9e4ffc3645cd66256
Daniel Jowett <daniel@jowettenterprises.com>
2015-04-01 20:55:05 +0100

    Remove unnecessary code for Overlay widget to work

--- src/covertile/cycle2/tiles/configuration_widgets/overlaytextarea.py
+++ src/covertile/cycle2/tiles/configuration_widgets/overlaytextarea.py
@@ -18,20 +18,9 @@ class IOverlayTextAreaWidget(interfaces.ITextAreaWidget):
 class OverlayTextAreaWidget(TextAreaWidget):
     """Textarea widget for Cycle2 Overlay field implementation."""
 
-    def update(self):
-        """See z3c.form.interfaces.IWidget."""
-        super(TextAreaWidget, self).update()
-        widget.addFieldClass(self)
-        self.value = self.context.get('overlay')
 
-@zope.component.adapter(zope.schema.interfaces.IField,
-                        zope.interface.Interface, interfaces.IFormLayer)
+@zope.component.adapter(zope.schema.interfaces.IField, interfaces.IFormLayer)
 @zope.interface.implementer(interfaces.IFieldWidget)
-def OverlayTextAreaFieldWidget(field, source, request=None):
-    """IFieldWidget factory for OverlayTextAreaWidget."""
-    # BBB: emulate our pre-2.0 signature (field, request)
-    if request is None:
-        real_request = source
-    else:
-        real_request = request
-    return FieldWidget(field, OverlayTextAreaWidget(real_request))
+def OverlayTextAreaFieldWidget(field, request):
+    """IFieldWidget factory for OverlayTextWidget."""
+    return FieldWidget(field, OverlayTextAreaWidget(request))
