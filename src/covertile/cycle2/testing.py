b2f008577b5f43865222cddd7f18157b4b076164
hvelarde <hector.velarde@gmail.com>
2016-06-21 10:47:54 -0300

    Depend on [test] extra from collective.cover
    Also, some refactoring on test setup.

--- src/covertile/cycle2/testing.py
+++ src/covertile/cycle2/testing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from collective.cover.testing import generate_jpeg
 from collective.cover.tests.utils import create_standard_content_for_tests
-
+from collective.cover.tests.utils import set_image_field
 from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
@@ -24,23 +24,22 @@ class Fixture(PloneSandboxLayer):
         self.applyProfile(portal, 'collective.cover:default')
         self.applyProfile(portal, 'collective.cover:testfixture')
         self.applyProfile(portal, 'covertile.cycle2:default')
+
+        # setup test content
         create_standard_content_for_tests(portal)
-        portal['my-image1'].setImage(generate_jpeg(900, 400))
-        portal['my-image1'].reindexObject()
-        portal['my-image2'].setImage(generate_jpeg(900, 400))
-        portal['my-image2'].reindexObject()
+        set_image_field(portal['my-image1'], generate_jpeg(900, 400))
+        set_image_field(portal['my-image2'], generate_jpeg(900, 400))
 
 
 FIXTURE = Fixture()
 
 INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,),
-    name='covertile.cycle2:Integration')
+    bases=(FIXTURE,), name='covertile.cycle2:Integration')
 
 FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE,),
-    name='covertile.cycle2:Functional')
+    bases=(FIXTURE,), name='covertile.cycle2:Functional')
 
 ROBOT_TESTING = FunctionalTesting(
     bases=(FIXTURE, AUTOLOGIN_LIBRARY_FIXTURE, z2.ZSERVER_FIXTURE),
-    name='covertile.cycle2:Robot')
+    name='covertile.cycle2:Robot',
+)
